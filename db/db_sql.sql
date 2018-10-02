1a2068927508e0cd6bc41cf9d56c20f3ba841ccd
Guilherme Alves <guilherme.alves3340@gmail.com>
2018-10-02 12:27:48 -0300

    Correções

--- db/db_sql.sql
+++ db/db_sql.sql
@@ -1,3 +1,16 @@
+
+/*
+CREATE TABLE dataset.tb_video(
+    --user_id         INTEGER CONSTRAINT nn_user_id NOT NULL,
+    path_file       VARCHAR(60) NOT NULL,
+    data_postagem   DATETIME NOT NULL,
+    id_video        DATETIME NOT NULL,
+    CONSTRAINT fk_video_user_id FOREIGN KEY(user_id)
+        REFERENCES dataset.tb_usuario(user_id)
+);*/
+
+DROP SCHEMA dataset;
+
 CREATE SCHEMA dataset;
 
 CREATE TABLE dataset.tb_usuario(
@@ -18,16 +31,23 @@ CREATE TABLE dataset.tb_usuario(
     CONSTRAINT pk_user_id PRIMARY KEY(user_id)
 );
 
-
-CREATE TABLE dataset.tb_video(
-    user_id         INTEGER CONSTRAINT nn_user_id NOT NULL,
-    path_file       VARCHAR(60) NOT NULL,
-    data_postagem   DATETIME NOT NULL,
-    id_video        DATETIME NOT NULL,
-    CONSTRAINT fk_video_user_id FOREIGN KEY(user_id)
-        REFERENCES dataset.tb_usuario(user_id)
-);
-
 CREATE TABLE dataset.tb_modulos(
     disponivel      INTEGER NOT NULL
 );
+
+
+CREATE TABLE dataset.tb_video(
+    path_file VARCHAR(60) NOT NULL,
+    data_postagem DATETIME NOT NULL,
+    id_video DATETIME NOT NULL
+);
+
+ALTER TABLE dataset.tb_video ADD COLUMN fk_video_user_id INTEGER;
+
+ALTER TABLE dataset.tb_video ADD FOREIGN KEY(fk_video_user_id)
+REFERENCES dataset.tb_usuario(user_id); 
+
+describe dataset.tb_video;
+describe dataset.tb_usuario;
+describe dataset.tb_modulos;
+
