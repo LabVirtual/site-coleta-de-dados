58a4132081500222eee6890066edc11fed94eeff
Guilherme Alves <guilherme.alves3340@gmail.com>
2018-10-02 09:41:36 -0300

    Sem grandes alterações

---
+++ db/db_sql.sql
@@ -0,0 +1,33 @@
+CREATE SCHEMA dataset;
+
+CREATE TABLE dataset.tb_usuario(
+    nome 			VARCHAR(45) NOT NULL,
+    sobrenome 		VARCHAR(45) NOT NULL,
+    user_id 		INTEGER,
+    cpf 			INTEGER ,
+    senha 			VARCHAR(15) NOT NULL,
+    email 			VARCHAR(45),
+    sexo 			BOOLEAN NOT NULL,
+    nascionalidade 	VARCHAR(15) NOT NULL,
+    cidade 			VARCHAR(45) NOT NULL,
+    estado 			CHAR(2) NOT NULL,
+    modulo_atual 	INTEGER,
+    num_video_atual INTEGER,
+	dt_nascimento 	DATETIME NOT NULL,
+    dt_cadastro 	DATETIME NOT NULL,
+    CONSTRAINT pk_user_id PRIMARY KEY(user_id)
+);
+
+
+CREATE TABLE dataset.tb_video(
+    user_id         INTEGER CONSTRAINT nn_user_id NOT NULL,
+    path_file       VARCHAR(60) NOT NULL,
+    data_postagem   DATETIME NOT NULL,
+    id_video        DATETIME NOT NULL,
+    CONSTRAINT fk_video_user_id FOREIGN KEY(user_id)
+        REFERENCES dataset.tb_usuario(user_id)
+);
+
+CREATE TABLE dataset.tb_modulos(
+    disponivel      INTEGER NOT NULL
+);
