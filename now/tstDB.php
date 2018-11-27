c0c3ac750ef8bdded18ee7d42e7df6f8f81e7feb
Guilherme Alves <guilherme.alves3340@gmail.com>
2018-11-27 11:04:28 -0200

    UPDATE

--- now/tstDB.php
+++ now/tstDB.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "******";
+$password = "guilherme";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password);
